64469fc115e05831ce809eb55cf0488d0dd727ba
walidad007 <walidadmahtam62@gmail.com>
2026-06-29 22:57:16 +0500

    update text_splitter.py

--- server/rag/text_splitter.py
+++ server/rag/text_splitter.py
@@ -1,4 +1,5 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from server.config import CHUNK_SIZE, CHUNK_OVERLAP
 
 
 def split_documents(documents):
@@ -6,7 +7,7 @@ def split_documents(documents):
     Split documents into chunks.
     """
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
 
     chunks = splitter.split_documents(documents)
 
