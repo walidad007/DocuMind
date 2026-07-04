e3de859335ffa7dc975ce29f838cee248d6bddcf
walidad007 <walidadmahtam62@gmail.com>
2026-07-04 22:33:46 +0500

    update rag_chain.py

--- server/rag/rag_chain.py
+++ server/rag/rag_chain.py
@@ -1,28 +1,29 @@
 import os
 from dotenv import load_dotenv
-
-# from langchain.chains.retrieval_qa.base import RetrievalQA
-
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
-
-# from langchain.prompts import PromptTemplate
 from server.rag.retriever import get_retriever
+from server.logger import logger
 
+# Load environment variables from .env file
 load_dotenv()
 
 
 def build_rag_chain():
-    """
-    Build RAG chain.
-    """
+    """Assemble the final RAG (Retrieval-Augmented Generation) pipeline."""
+    logger.info("Starting RAG chain building process...")
 
+    # Initialize the high-performance Llama-3.3 LLM using ChatGroq
     llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model="llama-3.3-70b-versatile")
 
+
+    # Load our configured document retriever
     retriever = get_retriever()
 
+    # Combine the LLM and Retriever into a complete Question-Answering (QA) chain
     qa_chain = RetrievalQA.from_chain_type(
-        llm=llm, retriever=retriever, return_source_documents=True
+        llm=llm, retriever=retriever, return_source_documents=True # Keeps track of which PDF page the answer came from
     )
 
+    logger.info("RAG chain built successfully and ready for queries.")
     return qa_chain
